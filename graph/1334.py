195682768c12f7b12c7d1c8f580071027b18b160
dorahkim <best8388@gmail.com>
2020-08-16 22:30:37 +0900

    Create 1334.py

---
+++ graph/1334.py
@@ -0,0 +1,33 @@
+#1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance
+
+class Solution:    
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        
+        dists = [[distanceThreshold + 1 for i in range(n)] for j in range(n)]
+        for s, e, w in edges:
+            dists[s][e] = w
+            dists[e][s] = w
+        
+        for i in range(n):
+            dists[i][i] = 0        
+        
+        #All Pairs Shortest Path: Floyd-Warshall Algorithm
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if dists[i][k] + dists[k][j] > distanceThreshold:
+                        continue
+                    elif dists[i][j] is None:
+                        dists[i][j] = dists[i][k] + dists[k][j]
+                    elif dists[i][k] + dists[k][j] < dists[i][j]:
+                        dists[i][j] = dists[i][k] + dists[k][j]
+    
+        #smallest number of neighbors
+        mn = n
+        city = None
+        for i in range(n):
+            cnt = len([j for j in dists[i] if j <= distanceThreshold])
+            if cnt <= mn:
+                mn = cnt
+                city = i
+        return city
